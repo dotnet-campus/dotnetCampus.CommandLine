49c5b220f41ce7a1798678b41924c5947cbfd209
walterlv <xblero@gmail.com>
2020-05-27 13:42:19 +0800

    Add analyzer project and pack it into the nuget package.

---
+++ tests/dotnetCampus.CommandLine.Analyzer.Test/dotnetCampusCommandLineAnalyzerUnitTests.cs
@@ -0,0 +1,65 @@
+﻿//using Microsoft.CodeAnalysis;
+//using Microsoft.CodeAnalysis.CodeFixes;
+//using Microsoft.CodeAnalysis.Diagnostics;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using dotnetCampus.CommandLine.Analyzer;
+//using Verify = Microsoft.CodeAnalysis.CSharp.CodeFix.Testing.MSTest.CodeFixVerifier<
+//    dotnetCampus.CommandLine.Analyzer.dotnetCampusCommandLineAnalyzerAnalyzer,
+//    dotnetCampus.CommandLine.Analyzer.dotnetCampusCommandLineAnalyzerCodeFixProvider>;
+
+//namespace dotnetCampus.CommandLine.Analyzer.Test
+//{
+//    [TestClass]
+//    public class UnitTest
+//    {
+//        //No diagnostics expected to show up
+//        [TestMethod]
+//        public async Task TestMethod1()
+//        {
+//            var test = @"";
+
+//            await Verify.VerifyCSharpDiagnosticAsync(test);
+//        }
+
+//        //Diagnostic and CodeFix both triggered and checked for
+//        [TestMethod]
+//        public async Task TestMethod2()
+//        {
+//            var test = @"
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using System.Text;
+//    using System.Threading.Tasks;
+//    using System.Diagnostics;
+
+//    namespace ConsoleApplication1
+//    {
+//        class TypeName
+//        {   
+//        }
+//    }";
+
+//            var fixtest = @"
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using System.Text;
+//    using System.Threading.Tasks;
+//    using System.Diagnostics;
+
+//    namespace ConsoleApplication1
+//    {
+//        class TYPENAME
+//        {   
+//        }
+//    }";
+
+//            var expected = Verify.Diagnostic("dotnetCampusCommandLineAnalyzer").WithLocation(11, 15).WithArguments("TypeName");
+//            await Verify.VerifyCSharpFixAsync(test, expected, fixtest);
+//        }
+//    }
+//}
