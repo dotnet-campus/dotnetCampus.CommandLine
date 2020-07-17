00c21000e3e89c77c3c13468544a8f726d313b4c
walterlv <xblero@gmail.com>
2020-07-17 12:03:55 +0800

    添加示例项目

---
+++ samples/dotnetCampus.CommandLine.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using dotnetCampus.Cli.Properties;
+
+namespace dotnetCampus.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parse(args, LocalizableStrings.ResourceManager)
+                .AddHandler<SampleOptions>(o => o.Run())
+                .Run();
+        }
+    }
+}
