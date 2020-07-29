0cbd08b78e06e6404e1c5cc93f5a901d223ead54
walterlv <xblero@gmail.com>
2020-07-29 20:27:25 +0800

    消除警告

--- src/dotnetCampus.CommandLine/Compatibility/LegacyFrameworkCompatibility.cs
+++ src/dotnetCampus.CommandLine/Compatibility/LegacyFrameworkCompatibility.cs
@@ -19,7 +19,9 @@ namespace dotnetCampus.Cli.Compatibility
 #if NETCOREAPP3_0 || NETCOREAPP3_1 || NETCOREAPP5_0 || NET5_0 || NET6_0
             return Array.Empty<T>();
 #else
+#pragma warning disable CA1825 // 避免长度为零的数组分配。
             return new T[0];
+#pragma warning restore CA1825 // 避免长度为零的数组分配。
 #endif
         }
     }
